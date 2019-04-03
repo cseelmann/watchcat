7ba97ab73f70604e1c7857ded8e59f92c8fdbbed
cseelmann <44268550+cseelmann@users.noreply.github.com>
2019-04-03 18:50:47 +0200

    Update watchcat.sh
    Added openssl check in addition to netcat, as ip tunneling might appear as open port for netcat. openssl verifies actual SSL availability of service

--- watchcat.sh
+++ watchcat.sh
@@ -4,9 +4,14 @@
 host='123.456.789.0'
 port='443'
 
+#method, either openssl or netcat. Openssl might be useful for https or for ip tunneling
+method='openssl'
+
 #timeouts = number of failed connections; interval = number of seconds to wait for new test
 timeouts=9
 interval=600
+
+## do not edit below this line
 
 script="${0##*/}"
 for pid in $(pidof -x $script); do
@@ -19,9 +24,19 @@ done
 declare -i away_counter=0
 
 while true; do
-        echo "Probing host: $host on port $port..."
-        [[ `nc -w 2 -zv $host $port 2>&1 |grep -c succeeded` -eq 0 ]] && echo "Host is down" && away_counter=$((away_counter+1))
+        echo "Probing host: $host on port $port with method $method..."
+
+        case $method in
+                openssl)
+                        [[ `openssl s_client -connect $host:$port 2>&1 |grep -c errno=104` -eq 1 ]] && echo "Host is down" && away_counter=$((away_counter+1))
+                ;;
+                netcat)
+                        [[ `nc -w 2 -zv $host $port 2>&1 |grep -c succeeded` -eq 0 ]] && echo "Host is down" && away_counter=$((away_counter+1))
+                ;;
+        esac
+
         sleep $interval
+        echo $away_counter
         if [ $away_counter == $timeouts ]; then
                 echo "Rebooting..."
                 sudo /sbin/reboot
